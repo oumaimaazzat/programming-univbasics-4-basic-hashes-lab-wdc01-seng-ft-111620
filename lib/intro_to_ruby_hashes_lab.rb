d085096985c35fe33ed4136cc7214892d08bf218
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-08 02:48:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,17 @@
 def new_hash
-  # return an empty hash
+  {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  Hash.new
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+ hash = {}
+ hash
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  { id: number}
 end
